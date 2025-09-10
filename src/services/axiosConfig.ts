af200b430c6c8a25e21db4267ed2cc120298368f
JianDevPeru <developer.jian@gmail.com>
2025-09-10 13:40:47 -0500

    .

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -8,6 +8,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 // ✅ Interceptor para añadir el token a cada solicitud si está presente
